741fae41a6a3552bc20162e0ed3d41aff15f6a7b
IvanAppSolution <vanz.net@gmail.com>
2025-10-29 15:06:29 +0800

    adjust auth guard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -7,28 +7,21 @@ import {
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
-import { IS_PUBLIC_KEY } from './decorators/allow-public.decorator';
-import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(
     private jwtService: JwtService,
     private configService: ConfigService,
-    private reflector: Reflector,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
+    const request = context.switchToHttp().getRequest();
 
-    if (isPublic) {
+    if (request.headers['x-public-route'] === 'true') {
       return true;
     }
 
-    const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
       throw new UnauthorizedException('No token provided');
