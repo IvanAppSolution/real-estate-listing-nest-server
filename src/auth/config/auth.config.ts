940b2ecbb2f0802ac4f49ff55ff40c021362fa91
IvanAppSolution <vanz.net@gmail.com>
2025-11-11 16:25:43 +0800

    update the token signature

--- src/auth/config/auth.config.ts
+++ src/auth/config/auth.config.ts
@@ -4,6 +4,6 @@ export default registerAs('auth', () => ({
     secret: process.env.JWT_TOKEN_SECRET,
     expiresIn: parseInt(process.env.JWT_TOKEN_EXPIRESIN ?? '3600', 10),
     refreshTokenExpiresIn: parseInt(process.env.REFRESH_TOKEN_EXPIRESIN ?? '86400', 10),
-    audience: process.env.JWT_TOKEN_AUDIENCE,
-    issuer: process.env.JWT_TOKEN_ISSUER
+    // audience: process.env.JWT_TOKEN_AUDIENCE,
+    // issuer: process.env.JWT_TOKEN_ISSUER
 }))
